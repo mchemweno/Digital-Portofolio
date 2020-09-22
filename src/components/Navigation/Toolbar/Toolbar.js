79707d4fb98d9dc8f26d48211f06b122bf228377
canary <mchemweno27@gmail.com>
2020-09-22 23:32:56 +0300

    meta edit

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,11 @@
 import React from "react";
 import classes from './Toolbar.module.css';
-import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 import Logo from "../../../UI/Logo/Logo";
 
 const Toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
                 <Logo/>
-        <DrawerToggle clicked={props.clicked}/>
         </header>
     )
 }
