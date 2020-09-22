64597403968ffbcd78bf942a6823d584af1911c9
canary <mchemweno27@gmail.com>
2020-09-22 23:45:44 +0300

    linkedin added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from './Footer.module.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faFacebookF, faInstagram, faTwitter, faWhatsapp} from '@fortawesome/free-brands-svg-icons'
+import {faFacebookF, faInstagram, faLinkedin, faTwitter, faWhatsapp} from '@fortawesome/free-brands-svg-icons'
 import {faAt} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = (props) => {
@@ -15,6 +15,7 @@ const Footer = (props) => {
             <a href={"mailto:mchemweno27@gmail.com?subject=Let%20us%20work%20together."}><FontAwesomeIcon icon={faAt} color={'rgba(1, 92, 103, 1)'} /></a>
             <a href={"https://twitter.com/_canary69"}><FontAwesomeIcon icon={faTwitter} color={'rgba(1, 92, 103, 1)'}/></a>
             <a href={'https://wa.me/254726990002'}><FontAwesomeIcon icon={faWhatsapp} color={'rgba(1, 92, 103, 1)'}/></a>
+            <a href={'https://www.linkedin.com/in/mark-chemweno-00bb65117'}><FontAwesomeIcon icon={faLinkedin} color={'rgba(1, 92, 103, 1)'}/></a>
         </p>
         <p className={classes.IncP}>Powered by CANARY&trade; & React.</p>
     </div>
